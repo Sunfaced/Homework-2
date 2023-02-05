1acd4d78c939e6868b27269e51ba08c59dba615d
Alexander <akoceryzhkin@gmail.com>
2023-02-05 22:34:16 +0300

    first

---
+++ main.js
@@ -0,0 +1,91 @@
+"use strict";
+
+// Пример 1
+
+// let minute = 60;
+// if (minute > 0 && minute <= 14){
+//     console.log('Первая четверть часа');
+// }else if (minute > 14 && minute <= 29){
+//     console.log('Вторая четверть часа')
+// }else if (minute > 29 && minute <= 44){
+//     console.log('Третья четверть часа')
+// }else if (minute > 44 && minute <= 59){
+//     console.log('Четвертая четверть часа')
+// }else 
+//     console.log("Ошибка");
+
+// Пример 2
+
+// let arr;
+// let lang = 'ru';
+// if (lang == 'ru'){
+//     let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+//     console.log(arr);
+// }
+// if (lang == 'en'){
+//     let arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+//     console.log(arr);
+// }
+
+// Пример 3
+
+// let i = prompt('Введите целое число');
+// if (i > 0 && i <= 9){
+//     alert('Однозначное ' + 'Положительное число');
+// }else if(i >= 10 && i <= 99){
+//     alert('Двузначное ' + 'Положительное число');
+// }else if(i >= 100 && i <= 999){
+//     alert('Трехзначное ' + 'Положительное число');
+// }else if(i < 0 && i >= -9){
+//     alert('Однозначное ' + 'Отрицательное число');
+// }else if(i <= -10 && i >= -99){
+//     alert('Двузначное ' + 'Отрицательное число');
+// }else if(i <= -100 && i >= -999){
+//     alert('Трехзначное ' + 'Отрицательное число');
+// }else 
+//     alert('Ошибка');
+
+// Пример 4
+
+// let red = 'нет';
+// let yellow = 'нет';
+// if (red  == 'да'){
+//     console.log('Stop');
+// }else if (yellow == 'да'){
+//     console.log('Stop');
+// }else if (red  == 'нет'){
+//     console.log('GO');
+// }else if (yellow  == 'нет'){
+//     console.log('GO');
+// }
+
+// Пример 5
+
+// let i = 1;
+// while(i<13){
+//     console.log(i);
+//     i++;
+// }
+
+// Пример 6
+
+// let i = 5;
+// while(i<14){
+// console.log(i);
+// i++;
+// }
+
+// Пример 7
+
+// for(let i = 2; i < 17; i += 2){
+//     console.log(i);
+// }
+
+// Пример 8
+
+// let arr = [2,5,8,3,6];
+// let result = 0;
+// for (let i = 0; i < arr.length; i++){
+//   result += arr[i];
+// }
+// console.log(result);
